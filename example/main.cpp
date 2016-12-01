d3dbdb605331fac0d66e9ec6144aab1346819e15
rav0215 <turuntzeva.anastasiya@yandex.ru>
2016-12-02 02:10:04 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
-
-#include "calculator.hpp"
-
-int main()
-{
-    int x, y;
-    std::cin >> x;
-    std::cin >> y;
-    
-    std::cout << sum( x, y ) << std::endl;
+#include <cstdlib>
+using namespace std;
+int main (){
+    setlocale(LC_ALL, "Russian");
+    char ss;
+    do
+    {
+        cout << "Enter the number" << endl;
+        float x, y, r;
+        char o;
+        cin >> x;
+        cin >> o;
+        cin >> y;
+        switch (o)
+        {
+            case '+':
+                r = x + y;
+                break;
+            case '-':
+                r = x - y;
+                break;
+            case '*':
+                r = x * y;
+                break;
+            case '/':
+                r = x / y;
+                break;
+        }
+        cout << x << o << y << '=' << r << endl ;
+        cout << " Continue? ( Y/N )" << endl;
+        cin >> ss;
+    }
+    while ( ss == 'y');
+    return EXIT_SUCCESS;
 }
+ 
