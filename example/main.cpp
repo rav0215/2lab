13bda764b68bcb0c3f75325ab2175bad8a7f5b74
rav0215 <turuntzeva.anastasiya@yandex.ru>
2016-12-08 23:01:37 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -2,30 +2,38 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
+setlocale(LC_ALL, "Russian");
+
+
+double sum (double x, double y);
+double subtraction (double x, double y);
+double mult (double x, double y);
+double division (double x, double y);
+double power (double x);
+double sqrt (double x);
+ 
+
 int main ()
 {
-double x, y; 
-int r;
-cin >> x;
-cin >> y;
-cin >> r;
-setlocale(LC_ALL, "Russian");
-double sum(double x, double y);
-double subtraction(double x, double y);
-double mult(double x, double y);
-double division(double x, double y);
-double power(double x, int y);
-double sqrt(double x);
-if (y==0)
-    cout << " Banned act "<< endl;
+    double x, y;
+    cout << " Enter x ";
+    cin >> x;
+    cout << " Enter y ";
+    cin >> y;
+    cout << " x + y =  " << sum ( x , y ) << endl;
+    cout << " x - y =  " << subtraction ( x , y ) << endl;
+    cout << " x * y =  " << mult ( x , y ) << endl;
+    if ( y == 0 )
+        cout << " x / y = Error " << endl;
     else 
-        cout << "double division(double x, double y)"<< endl;
-    if (x<=0)
-       cout << " No roots ";
+        cout << " x / y = " << division ( x , y ) << endl;
+    cout << " x ^ y  = " << power ( x , y ) << endl;
+    if ( x < 0 )
+        cout << " sqrt (x) = no roots " << endl;
     else 
-        cout << "double sqrt(double x)" << endl;
+        cout << " sqrt (x) = " << sqrt ( x , y ) << endl;
+    
     return EXIT_SUCCESS;
+    
 }
-
-
 
